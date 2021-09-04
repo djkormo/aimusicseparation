c9a8f6a136e287729cd53ceab0469080b86398f5
qooba <dev@qooba.net>
2021-09-05 00:28:27 +0200

    change run.sh

--- run.dev.sh
+++ run.dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --name aiaudioseparation -it -p 8000:8000 -p 8888:8888 --rm -v $(pwd)/src/app:/app  qooba/aimusicseparation:dev
+docker run --name aiaudioseparation --gpus all -it -p 8000:8000 -p 8888:8888 --rm -v $(pwd)/src/app:/app  qooba/aimusicseparation:dev
 #docker run --name aiscissors -it -p 8000:8000 --rm -v $(pwd)/src/app:/app -v $(pwd)/u2net_models:/root/.u2net  qooba/aiscissors ./start-reload.sh
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run --name aiaudioseparation -it -p 8000:8000 -v $(pwd)/checkpoints:/root/.cache/torch/hub/checkpoints --rm qooba/aimusicseparation
+#CPU
+#docker run --name aiaudioseparation -it -p 8000:8000 -v $(pwd)/checkpoints:/root/.cache/torch/hub/checkpoints --rm qooba/aimusicseparation
+
+#GPU
+docker run --name aiaudioseparation --gpus all -it -p 8000:8000 -v $(pwd)/checkpoints:/root/.cache/torch/hub/checkpoints --rm qooba/aimusicseparation
